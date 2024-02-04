926593b337ffda5f5619a11ae8f84c2acd4b30a4
Gyse-Gidra <evginich2610@gmail.com>
2024-02-04 18:15:19 +0300

    prime data types

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello project from Platon")
+num = 1 # int
+num2 = 1.0 # float
+str = "hello" # str или string
+boolean = True # bool, true or false, 1 or 0)
+
+print("num")
+print("num2")
+print("str")
+print("boolean")
+
