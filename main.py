b8256ba512c0cbf3e614f0b09bc2ea5a48765278
MironKrasavchikoff <harlam1981.5@gmail.com>
2024-02-04 18:10:49 +0300

    prime data types

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello project from Miroslav")
+num = 1 # int
+num2 = 1.0 # float
+str = "hello" # str или string
+boolean = True # bool, true or false, 1 or 0
+
+print(num)
+print(num2)
+print(str)
+print(boolean)
