1ae0b41bdda8600d9b42031c0ad1d24c28a5493c
m5hut <m5555hutov@gmail.com>
2024-02-04 18:10:04 +0300

    group 2 prime data types

---
+++ main.py
@@ -0,0 +1,11 @@
+num = 1 # int
+num2 = 1.0 # float
+str = "Hello" # str или String
+boolean = True # bool, true or false, 1 or 0]
+
+print(num)
+print(num2)
+print(str)
+print(boolean)
+
+# prime data types
