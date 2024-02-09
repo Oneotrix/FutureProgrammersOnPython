820ca58e11f711b4f2a7123149c6f82f0ded5793
dayte <daytefake@gmail.com>
2024-02-09 17:26:34 +0500

    find all prime numbers in a given range.

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-N = int(input())
-sum = 0
-i = 0
+firstNum = int(input())
+secondNum = int(input())
+def primeNum(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
-while i <= N:
-    if i % 2 == 0:
-        sum += i
-    i += 1
+def primeRange(firstNum, secondNum):
+    primeNumbers = []
+    for num in range(firstNum, secondNum + 1):
+        if primeNum(num):
+            primeNumbers.append(num)
+    return primeNumbers
 
-print(sum)
+primeNumbers = primeRange(firstNum, secondNum)
+print(primeNumbers)
+
