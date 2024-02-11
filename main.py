95967853dcc74bd2f6c5ba857d4aecc123e64990
m5hut <m5555hutov@gmail.com>
2024-02-11 17:56:36 +0300

    refactor lesson 3 task 1

--- main.py
+++ main.py
@@ -1,36 +1,5 @@
-import time
-
-num1 = 4
-num2 = 12
-out = num1 + num2
-print("4+12 =",out)
-out = num1 - num2
-print("4-12 =",out)
-out = num1 * num2
-print("4*12 =",out)
-out = num2 / num1
-print("12/4 =",out)
-out = num1 ** num2
-print("4**12 =",out)
-out = num1 % num2
-print("4%12 =",out)
-out = num1 // num2
-print("4//12 =",out)
-out = num1 / (1/2)
-print (out)
-
-
-
-
 K = 6
 N = 24
-if K < N :
-    K=K+1
-    while K<=(N-1) :
-        print(K)
-        K=K+1
-else:
-    N =N +1
-    while N <= (K-1):
-        print(N)
-        N=N + 1
+while K <= N:
+    print(K)
+    K+=1
