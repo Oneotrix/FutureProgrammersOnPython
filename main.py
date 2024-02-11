f483ec28244f254beee6a3133f376435196e48d3
MironKrasavchikoff <harlam1981.5@gmail.com>
2024-02-11 17:17:07 +0300

    математические операции

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-num = 1 # int
-num2 = 1.0 # float
-str = "hello" # str или string
-boolean = True # bool, true or false, 1 or 0
-
-print(num)
-print(num2)
-print(str)
-print(boolean)
+a=1
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a%b)
+print(a//b)
