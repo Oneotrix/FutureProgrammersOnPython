c3df64700c69651f6f72f4525fedde8845ef9c7d
ChopChopIDK <wotssde@gmail.com>
2024-02-11 18:16:55 +0300

    Lesson3 Task2

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 K = int(input("Введи число K: "))
 N = int(input("Введи число N: "))
 
-for i in range(K + 1, N):
-    print(i)
+if K<N :
+    for i in range(K + 1, N):
+        print(i)
+else:
+    for i in range(N + 1, K):
+        print(i)
