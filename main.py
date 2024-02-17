cc8b7eff07e8ce3d23d52e4c4ac897c588cbf859
bobrcro <mihailhodorevsky@yandex.ru>
2024-02-17 19:25:28 +0300

    Контрольная работа 1 задание 11

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-a = int(input())
-a = a = 1, a > 1
-b = int(input())
-b = b =0, b < 3
-
-sum = a + b
-
-tru = a
-tru1 = b
-
-print(tru)
-print(sum)
-print(tru1)
+n = int(input("Введите целое число : "))
+i = 0
+while i <= n:
+ print(i)
+i += 1
