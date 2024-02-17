53701882cb67b9f2ab950cb8c1a58eb7814ed8e6
dayte <daytefake@gmail.com>
2024-02-17 18:15:10 +0500

    контрольная работа 1 номер 1

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-numbers = [4, 2, 7, 1, 5, 3, 6, 8, 9, 10]
-
-def sortNumbers(numbers, sorting):
-    sorted_numbers = sorting(numbers)
-    return sorted_numbers
-
-sortedNumbers = sortNumbers(numbers, lambda x: sorted(x))
-
-print(sortedNumbers)
+variable = 3
+print(variable)
