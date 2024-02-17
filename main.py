c15a042711292c9a3641ccc8eb359359314dc58c
bobrcro <mihailhodorevsky@yandex.ru>
2024-02-17 18:12:15 +0300

    Контрольная работа 1

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-low = int(input("введите нижнею границу деапозона "))
-high = int(input("введите вернюю границу деапозона "))
-a = []
-for x in range(low, high + 1):
-    i = 2
-    while x % i != 0:
-        i += 1
-    if i == x:
-        a.append(x)
-print(a)
+A = 6
+
+print(A)
