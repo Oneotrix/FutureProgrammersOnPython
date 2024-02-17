48a575558ff0a5f8e0f80de5e2782bd974b9d425
dayte <daytefake@gmail.com>
2024-02-17 18:16:02 +0500

    контрольная работа 1 номер 2

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-variable = 3
-print(variable)
+a = 10
+b = 2
+sum = a + b
+raz = a - b
+multi = a * b
+chastnoe = a // b
+chastnoe2 = a % b
+chastnoe3 = a / b
+
+print(sum)
+print(raz)
+print(multi)
+print(chastnoe)
+print(chastnoe2)
+print(chastnoe3)
