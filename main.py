2f0b2fc896e8b48b7c05ca47fceec787c4bf53ba
ggqls <namenkhakharov@gmail.com>
2024-02-18 17:12:58 +0300

    task4

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-int1 = 10
-int2 = 2
-result = int1 + int2  # result = 12
-result2 = int1 - int2  # result = 8
-result3 = int1 * int2  # result = 20
-result4 = int1 / int2  # result = 5
+a = 10
+b = 2
+c = 5
+result = a + b + c
+print("result", 17)
