87ff792f45790d790eb3aa03f8e5475ead387def
ChopChopIDK <wotssde@gmail.com>
2024-02-18 17:39:12 +0300

    Lesson4 Task7

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-name="Костя"
-age="15"
-num="96"
-print("меня зовут",name,"мне",age,"учусь в школе номер",num)
-
-K = int(input("Введи число K: "))
-N = int(input("Введи число N: "))
-
-if K<N :
-    for i in range(K + 1, N):
-        print(i)
+a=input()
+if a>0:
+    print("ВЕРНО")
 else:
-    for i in range(N + 1, K):
-        print(i)
+    print("НЕВЕРНО")
