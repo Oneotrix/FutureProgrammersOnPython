76355499001ba00038e02c64a6ee65e3cbd8c35e
ggqls <namenkhakharov@gmail.com>
2024-02-18 17:54:47 +0300

    task7

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-num = 36
-age = 13
-name = "намен"
-print("меня зовут", name, "мне", age, "я учусь в", num, "гимназии")
+a = int(input("Введите число : "))
+num1 = a
+num2 = 0
+if num1 > num2:
+    print('true')
+else:
+    print('false')
