8f61dbb13b5ae3f82024829c11e992ed9489862d
Gyse-Gidra <evginich2610@gmail.com>
2024-02-18 17:00:13 +0300

    lesson 4 task 1

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-num = 1 # int
-num2 = 1.0 # float
-str = "hello" # str или string
-boolean = True # bool, true or false, 1 or 0)
-
-print("num")
-print("num2")
-print("str")
-print("boolean")
-
+A = 3
+print("a = 3")
