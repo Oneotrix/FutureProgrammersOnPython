c7a79cd5896a91f57aee19c61715e1582e0d463c
just <misha_ar161@mail.ru>
2024-02-24 19:08:50 +0300

    5 функций - * / // %

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-def hello():
-    print("Привет")
-    print("Ваше имя")
-    print("Пока")
-hello()
+# - * / % //
+def minus(a: int,  b: int):
+    c = a - b
+    print(c)
+
+
+minus(8,7)
+
+def multiply(a: int,  b: int):
+    c = a * b
+    print(c)
+
+multiply(7, 7)
+
+def деление1(a: int,  b: int):
+    c = a / b
+    print(c)
+
+деление1(8, 8)
+
+def деление2(a: int,  b: int):
+    c = a // b
+    print(c)
+
+деление2(9, 9)
+
+def деление3(a: int,  b: int):
+    c = a % b
+    print(c)
+
+деление3(6, 6)
