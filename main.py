d35dd7fd4bce068f9576c2eab7996529197dc17c
bobrcro <mihailhodorevsky@yandex.ru>
2024-02-24 19:39:39 +0300

    функции 3

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-
-def minus(o: int, l: int):
-    c = o - l
+def cro(p: int, d: int):
+    c = p - d
     print(c)
 
-minus(5, 3)
+def cro1(v: int, y: int):
+    w = v + y
+    print(w)
+def cro2(m: int, j: int ):
+    z = m / j
+    print(z)
+
+
+def cro3(i: int, o: int):
+    p1 = i % o
+    print(p1)
+
+def cro4(u: int, a: int):
+    h = u // a
+    print(h)
