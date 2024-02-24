790d667fedce881220ab2955102fbfde5a20e0b5
dayte <daytefake@gmail.com>
2024-02-24 14:45:10 +0500

    Write a program that finds all such numbers that are divisible by 7 but not divisible by 5, in the range 2000 to 3200 (both included).

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 list = []
-nums1Sum = 0
-nums2Sum = 0
 
-for i in range(-10, 20):
-    list.append(i)
+for i in range(2000, 2301):
+    if i % 7 == 0 and i % 5 != 0:
+        list.append(str(i))
 
-for i in list:
-    if i <= 1:
-        nums1Sum += i
-    elif 3 < i < 8:
-        nums2Sum += i
-
-print(nums1Sum - nums2Sum)
+result = ' '.join(list)
+print(result)
