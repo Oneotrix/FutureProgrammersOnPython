3a019e4e01c3fa9daf8f59f4552c59d4d7e270cc
ChopChopIDK <wotssde@gmail.com>
2024-02-25 17:25:49 +0300

    Lesson5 Task2

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-print( "Координаты центр квадрата,")
-x0 = int(input("х:"))
-y0 = int(input("y:"))
-a = int(input("Длинна стороны квадрата:"))
-print( "Координаты точки А,")
-x = int(input("х:"))
-y = int(input("y:"))
-af = x0+a/2
-bf = x0-a/2
-afy = y0+a/2
-bfy = y0-a/2
-if bf<=x<=af:
-    if bfy <= y <= afy:
-        print("х принадлежит!")
-    else:
-        print("х не принадлежит!")
-else:
-    print("х не принадлежит!")
+nums = [1,3,4,223,65,1,45,12,76,8]
+dem = 0
+for num in nums:
+    dem=dem+num
+print(dem)
