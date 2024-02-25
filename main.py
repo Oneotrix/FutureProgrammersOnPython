e1c56a76fd45ac5bc948dcdf88d8088a94ac5787
ChopChopIDK <wotssde@gmail.com>
2024-02-25 17:08:07 +0300

    Lesson5 Task1

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-x = int(input("Координата х:"))
-a = 3
-b =12
-if a<=x<=b:
-    print("х принадлежит!")
+print( "Координаты центр квадрата,")
+x0 = int(input("х:"))
+y0 = int(input("y:"))
+a = int(input("Длинна стороны квадрата:"))
+print( "Координаты точки А,")
+x = int(input("х:"))
+y = int(input("y:"))
+af = x0+a/2
+bf = x0-a/2
+afy = y0+a/2
+bfy = y0-a/2
+if bf<=x<=af:
+    if bfy <= y <= afy:
+        print("х принадлежит!")
+    else:
+        print("х не принадлежит!")
 else:
     print("х не принадлежит!")
