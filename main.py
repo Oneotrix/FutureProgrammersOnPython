31721f05a46df12e784aa1b0c2201b38ff5ce4c7
dayte <daytefake@gmail.com>
2024-03-02 18:05:31 +0500

    Найти сумму всех целых чисел от 100 до 500.

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-number = int(input("Введите число: "))
-
-def factorial(num):
-    if num == 0 or num == 1:
-        return 1
-    else:
-        return num * factorial(num - 1)
-
-if number < 0:
-    print("Отрицательне число")
-else:
-    print("Факториал цисла", number, ":", factorial(number))
+sum = 0
+for i in range(100, 501):
+    sum += i
+print(sum)
