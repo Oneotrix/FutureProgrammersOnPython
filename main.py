b67ebb1b8d4591382d5aa1a2c015401f1e5a2359
dayte <daytefake@gmail.com>
2024-03-02 18:07:42 +0500

    Написать функцию, которая принимает 2 параметра(верхнюю и нижнюю границу ренджа), считает сумму чисел данного ренджа(диапазон значений От и До) и возвращает это результат. Результат вывести в консоль

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-sum = 0
-for i in range(100, 501):
-    sum += i
-print(sum)
+def calculateSumOfRange(start, end):
+    sum = 0
+    for i in range(start, end):
+        sum += i
+    print("sum:", sum)
+
+calculateSumOfRange(100, 501)
