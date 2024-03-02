94b6748f51efb2a1c76231fd83f4d3453c1b4b79
bobrcro <mihailhodorevsky@yandex.ru>
2024-03-02 18:19:41 +0300

    Найти сумму всех целых чисел от 100 до 500.

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-def cro(p: int, d: int):
-    c = p - d
-    print(c)
-
-def cro1(v: int, y: int):
-    w = v + y
-    print(w)
-def cro2(m: int, j: int ):
-    z = m / j
-    print(z)
-
-
-def cro3(i: int, o: int):
-    p1 = i % o
-    print(p1)
-
-def cro4(u: int, a: int):
-    h = u // a
-    print(h)
+a = 100
+s = a
+for i in range(a, 500):
+    s = i + s
+print(s)
