eee75f2b512514e8072b9b0e6eafe60145a32e27
MironKrasavchikoff <harlam1981.5@gmail.com>
2024-03-10 17:31:20 +0300

    lesson7, task2

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-N=int(input("введите число "))
-for i in range(0, N):
-    print(i+1)
+list=["52","ща","","ой","","8","","872"]
+a=0
+for i in list:
+    if i=="":
+        list.pop(a)
+    a+=1
+print(list)
 
