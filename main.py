05d7d3da833aeb8ec1762c95bdf8997aa501c557
ggqls <namenkhakharov@gmail.com>
2024-03-17 17:59:47 +0300

    lesson 8 task 1

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# Введите с клавиатуры число N, создайте список длиной N
-# #, выведите в консоль срез списка от 0 до N-10
-
-n = int(input("введите число:"))
-list = [n]
-for i in range(0, n):
-    print(i+1)
+a = int(input("введите число:"))
+count = 0
+while(a > 0):
+    count = count + 1
+    a = a // 10
+    print(count)
