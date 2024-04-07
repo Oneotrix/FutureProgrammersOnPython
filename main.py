4b5e5f94b949883849beac683be232f4829481b5
ggqls <namenkhakharov@gmail.com>
2024-04-07 17:26:35 +0300

    task 1

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 a = int(input("введите число:"))
-count = 0
-while(a > 0):
-    count = count + 1
-    a = a // 10
-    print(count)
+b = int(input("введите число:"))
+c = int(input("введите число:"))
+d = int(input("введите число:"))
+
+suma = a+d
+sumaA = b+c
+
+if suma == sumaA:
+    print("равно")
+else:
+    print("не равно")
