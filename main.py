83e2b0f27320e5a3376083a1f00a5440f3f44117
ChopChopIDK <wotssde@gmail.com>
2024-04-07 16:27:04 +0300

    Lesson10 Task1 (HW)

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-p = 0
-s = 0
-halfp = 0
-while True:
-    a = int(input("Ввидите сторону A: "))
-    b = int(input("Ввидите сторону B: "))
-    c = int(input("Ввидите сторону C: "))
-    if a+b>c and a+c>b and b+c>a:
-        p =a+b+c
-        print("Пиримите: ",p)
-        halfp=p/2
-        s =(halfp*(halfp-a)*(halfp-b)*(halfp-c))**0.5
-        print("Площадь:",s)
-        break
+list = []
+lista = []
+listb = []
+for k in range(1,101):
+    list.append(k)
+
+for n in list:
+    if n>40 and n<60:
+        listb.append(n)
     else:
-        print("Треугольник НЕ возможен! Попобуйте еще раз.")
+        lista.append(n)
+print(lista)
+print(listb)
