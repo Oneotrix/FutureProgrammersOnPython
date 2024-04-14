9a1650ad6cba145504c50d8d51f5bb4c66ea919d
MironKrasavchikoff <harlam1981.5@gmail.com>
2024-04-14 17:35:03 +0300

    lesson11, task2

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list = [3, 4, 10, 6, 12]
-a = 0
-l = len(list)
-for i in list:
-    a += i
-    b = a / l
-print("Cреднее арифметическое -", b)
-
+list = [10, 5, 13, 21, 42, 51, 61]
+count = 0
+for i in range(1, len(list)):
+    if list[i] > list[i-1]:
+        count += 1
+print(count)
