333251783328fe15984fea23b04faf113da21a96
Gyse-Gidra <evginich2610@gmail.com>
2024-04-21 17:11:31 +0300

    lesson 13 task 1

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-list = [1,2,3,4,5,6,7,8,9,10]
-count = 0
-size = 0
+g = input(int)
 
-list.__len__()
+d = g[::-1]
 
-for i in  range(1, list.__len__()):
-    if list[i] > list[i-1]:
-        count +=1
-
-print(count)
+if g == d:
+    print("True")
+else:
+    print("false")
