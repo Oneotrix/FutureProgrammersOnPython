b176b8d6bb2cc05ffd2b4f0eb22fe60af8167031
MironKrasavchikoff <harlam1981.5@gmail.com>
2024-04-21 17:00:19 +0300

    lesson12, task1

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list = [10, 5, 13, 21, 42, 51, 61]
-count = 0
-for i in range(1, len(list)):
-    if list[i] > list[i-1]:
-        count += 1
-print(count)
+a = input("Введите число: ")
+if a == a[::-1]:
+    print("Число-палиндром")
+else: 
+    print("Число-не палиндром")
